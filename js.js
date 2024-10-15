58a136934cd8998e442b05b849eb3f6f3bf233ac
Yazan <queider@proton.me>
2024-10-15 13:48:38 +0300

    Add scalable grid

--- js.js
+++ js.js
@@ -1,6 +1,23 @@
-const mainContainerDiv = document.querySelector("#mainDiv");
-const gridSquares = mainContainerDiv.querySelectorAll(".rowdiv div");
+const ROWS = 200;
+const COLUMNS = 200;
 
+const mainContainerDiv = document.querySelector("#mainContainerDiv");
+
+//creating rows
+for (let i = 0; i < ROWS; i++) {
+    const elementRow = document.createElement("div");
+    elementRow.classList.add("divRow");
+    mainContainerDiv.appendChild(elementRow);
+
+    for (let i = 0; i < COLUMNS; i++) {
+        const elementColumn = document.createElement("div");
+        elementColumn.classList.add("divColumn");
+        elementRow.appendChild(elementColumn);
+    }
+  }
+
+
+const gridSquares = mainContainerDiv.querySelectorAll(".divRow div");
 gridSquares.forEach((div) => {
     div.addEventListener("mouseover", () => {
         div.style.backgroundColor = "black";
