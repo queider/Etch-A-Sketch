28fe86c1665518c9fe911b2b8b0912a59f8d4411
Yazan <queider@proton.me>
2024-10-16 14:23:45 +0300

    Add toggle color button and typography

--- js.js
+++ js.js
@@ -20,11 +20,19 @@ for (let i = 0; i < ROWS; i++) {
 
 // drawing
 let seedIndex = 0;
-const colorsArray = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff"];
+let colorPalateIndex = 0;
+const colorsArray = [
+    ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff"],
+    ["#c7522a", "#e5c185", "#f0daa5", "#fbf2c4", "#b8cdab", "#74a892", "#008585", "#004343"],
+    ["#fdc921", "#fdd85d", "#feebaa", "#fffdf7", "#cceaf1", "#99d6ea", "#4d839a", "#003049"],
+    ["#003f5c", "#58508d", "#8a508f", "#bc5090", "#de5a79", "#ff6361", "#ff8531", "#ffa600"],
+    ["#21231b", "#414635", "#626950", "#828b6a", "#9ba384", "#b3bb9d", "#ccd3b6", "#e4eacf"]
+];
+
 const gridSquares = mainContainerDiv.querySelectorAll(".divRow div");
 gridSquares.forEach((div) => {
     div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = colorsArray[colorIndex = seedIndex % 8];
+        div.style.backgroundColor = colorsArray[colorPalateIndex % 5][seedIndex % 8];
         seedIndex++;
     });
 });
@@ -36,4 +44,9 @@ cleanCanvasButton.addEventListener("click", () => {
     gridSquares.forEach((div) => { 
         div.style.backgroundColor = "white";
     });
+});
+
+const toggleCanvasButton = document.querySelector(".toggleColorsButton");
+toggleCanvasButton.addEventListener("click", () => {
+    colorPalateIndex++;
 });
